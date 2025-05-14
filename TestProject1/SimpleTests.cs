ca267531c9f23562b7bf3e8f1e0660c33e0570a5
Cy4n1d3Blu3 <kamil.duda@sspb.pl>
2025-05-14 22:50:05 +0200

    dodanie szablonu testów, wprowadzenie ścieżki względnej do wczytywania plików, dodanie komentarzy z TODO

---
+++ TestProject1/SimpleTests.cs
@@ -0,0 +1,246 @@
+﻿using IDE.Parser;
+using IDE.PQLParser;
+
+namespace TestProject1;
+
+[TestClass]
+public sealed class SimpleTests
+{
+    private static QueryParser queryParser;
+
+    [ClassInitialize]
+    public static void ClassSetup(TestContext ctx)
+    {
+        CodeParser parser = new CodeParser($"{AppDomain.CurrentDomain.BaseDirectory}\\..\\..\\..\\SIMPLE.txt");
+        parser.ReadFile();
+        parser.Parse();
+        queryParser = new QueryParser();
+    }
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void KamilTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void EwaTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void FilipTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void JanTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void KasiaTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+
+
+    [DataTestMethod]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    [DataRow("", "")]
+    public void MaciekTests(string query, string expectedResult)
+    {
+        var result = queryParser.ParseQuery(query);
+        Assert.AreEqual(expectedResult, result.Trim());
+    }
+}
